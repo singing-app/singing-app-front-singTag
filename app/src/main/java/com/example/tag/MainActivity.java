44431c31204d0cae5b7c4ba390a93cd609612c95
kim429 <sujjin29@gmail.com>
2022-05-28 21:41:02 +0900

    [수정/추가] 김수진 리사이클러뷰 추가

--- app/src/main/java/com/example/tag/MainActivity.java
+++ app/src/main/java/com/example/tag/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.tag;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private MyRecyclerAdapter mRecyclerAdapter;
+    private ArrayList<FriendItem> mfriendItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        /* initiate adapter */
+        mRecyclerAdapter = new MyRecyclerAdapter();
+
+        /* initiate recyclerview */
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL,false));
+
+        /* adapt data */
+        mfriendItems = new ArrayList<>();
+        for(int i=1;i<=10;i++){
+                mfriendItems.add(new FriendItem(R.drawable.pancake,i+"번",i+"번째 제목",i+"번째 가수", i+"번째 음역대"));
+        }
+        mRecyclerAdapter.setFriendList(mfriendItems);
     }
 }
